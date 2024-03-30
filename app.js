f57feb91ddd550b7b724949cb88958d4708898ed
ibrahim abas <31131642+orolyana@users.noreply.github.com>
2024-03-30 05:36:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 
     var firebaseConfig = {
     apiKey: "AIzaSyDqpvwbY4ossT3g4MqtpcnJjHqGLGlIkUo",
-    authDomain: "AUTH DOMAIN",
+    authDomain: "safaricom-ethiopia-ce4eb.firebaseapp.com",
     databaseURL: "https://safaricom-ethiopia-ce4eb-default-rtdb.firebaseio.com/",
     projectId: "safaricom-ethiopia-ce4eb",
     storageBucket: "safaricom-ethiopia-ce4eb.appspot.com",
-    messagingSenderId: "SENDER ID",
+    messagingSenderId: "264200598009",
     appId: "1:264200598009:android:b25ad67a842ad878f50d28"
   }; 
  
