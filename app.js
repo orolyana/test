15a72ee48335667f32dc4f9bda93286edc4907a6
orolyana <ibrahimabas97@gmail.comm>
2024-03-30 05:31:56 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyDqpvwbY4ossT3g4MqtpcnJjHqGLGlIkUo",
+    authDomain: "AUTH DOMAIN",
+    databaseURL: "https://safaricom-ethiopia-ce4eb-default-rtdb.firebaseio.com/",
+    projectId: "safaricom-ethiopia-ce4eb",
+    storageBucket: "safaricom-ethiopia-ce4eb.appspot.com",
+    messagingSenderId: "SENDER ID",
+    appId: "1:264200598009:android:b25ad67a842ad878f50d28"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
